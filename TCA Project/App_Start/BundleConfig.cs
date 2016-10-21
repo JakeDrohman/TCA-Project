e9ceba792d23d7538cb9a785a21311818988418c
Boyce <joshuadboyce@gmail.com>
2016-10-21 10:21:15 -0500

    Updated Theme

--- TCA Project/App_Start/BundleConfig.cs
+++ TCA Project/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@ namespace TCA_Project
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap-theme.css",
+                      "~/Content/Site.css"));
         }
     }
 }
